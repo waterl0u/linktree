87b674b8a191fb8e68b35a72d57b003e89149946
waterl0u <jumalon.melanielou@gmail.com>
2024-10-15 11:50:36 -0600

    dom mani

--- arrays.js
+++ arrays.js
@@ -1,9 +1,14 @@
-let pets = ['dog', 'cat'];
+// get even numbers
+// function getEven(arr) {
+//     return arr.filter(number => number %2 === 0)
+// }
 
-console.log(pets
-    .map(function(pet) {
-    return pet.toUpperCase([pet[0]])
-}))
+// let pets = ['dog', 'cat'];
+
+// console.log(pets
+//     .map(function(pet) {
+//     return pet.toUpperCase([pet[0]])
+// }))
 
 // let petName = "Bingo";
 // const petName2 = petName.split('').reverse().join('');
---
+++ dom.js
@@ -0,0 +1,3 @@
+// function inputUpdated ()
+
+let myName = document.get
